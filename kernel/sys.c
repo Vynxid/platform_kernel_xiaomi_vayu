9807ac04e6a1e94c9ded36e5315efe5067c84f77
Vynxid <111081418+Vynxid@users.noreply.github.com>
2025-12-15 03:19:53 +0700

    Update sys.c

--- kernel/sys.c
+++ kernel/sys.c
@@ -1191,21 +1191,17 @@ static int override_release(char __user *release, size_t len)
 	return ret;
 }
 
-<<<<<<< HEAD
 static uint64_t netbpfload_pid = 0;
-=======
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
 
->>>>>>> e63d96f4e391 (BACKPORT: Implement SuSFS v2.0.0)
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
-<<<<<<< HEAD
 	if (!strncmp(current->comm, "netbpfload", 10) &&
 	    current->pid != netbpfload_pid) {
 		netbpfload_pid = current->pid;
@@ -1213,11 +1209,9 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 		pr_debug("fake uname: %s/%d release=%s\n",
 			 current->comm, current->pid, tmp.release);
 	}
-=======
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
        susfs_spoof_uname(&tmp);
 #endif
->>>>>>> e63d96f4e391 (BACKPORT: Implement SuSFS v2.0.0)
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
